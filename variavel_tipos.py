307518a244a05fbb8195ccd12ec9765f701c8acc
Gabriel Vieira <gabrielcnt96@gmail.com>
2025-09-11 23:33:06 -0300

    feat: criei e finalizei o arquivo variavel_tipos.py

---
+++ variavel_tipos.py
@@ -0,0 +1,13 @@
+# variavel do tipo int para representar idade
+idade = 7
+# variavel do tipo float para representar a altura
+altura = float(1.75)
+
+conversao = int(altura) # o valor decimal da altura é truncada, ou seja, será descartada
+print(conversao) # exibindo a conversão no terminal
+
+# variavel do tipo string para representar um nome
+nome = 'Gabriel'
+# variavel do tipo bool para representar se a ação é verdadeira ou falsa
+jogando = True
+
