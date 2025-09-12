ae33f52d53b03da3590ddd2cb7fc024a4eaf68d5
Gabriel Vieira <gabrielcnt96@gmail.com>
2025-09-12 19:10:20 -0300

    feat: exercicio controle_condicional.py finalizado

---
+++ controle_condicional.py
@@ -0,0 +1,13 @@
+nota = 2
+
+if nota >= 9 and nota <= 10: # Se a nota estiver entre 9 e 10, a condição retorna a nota A
+    print('nota A')
+
+elif nota >= 7 and nota < 9: # Se a nota estiver entre 7 e abaixo de 9, a condição retorna a nota B
+    print('nota B')
+
+elif nota >= 5 and nota < 7: # Se a nota estiver entre 5 e abaixo de 7, a condição retorna a nota C
+    print('nota C')
+
+else: # Se a nota for abaixo de 5, será retornada a nota D
+    print('nota D')
