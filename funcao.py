043d20bb29b7c8af2f20117182ed790a2fd2375a
Gabriel Vieira <gabrielcnt96@gmail.com>
2025-09-12 20:21:29 -0300

    feat: exercicio funcao.py finalizado

---
+++ funcao.py
@@ -0,0 +1,13 @@
+def soma(a,b):
+    '''
+    Esta função soma os valores dos 2 parâmetros
+    Parâmetros:
+    a (int): parametro para somar
+    b (int): parametro para somar
+
+    retorna:
+    soma dos valores dos parâmetros
+    '''
+    return a + b
+
+print(soma(10, 3))
