ddbab7126a65bafd56958d9d3f2626a9b098c9a5
Gabriel Vieira <gabrielcnt96@gmail.com>
2025-09-12 18:29:29 -0300

    feat: finalização do arquivo operacao_aritmeticas.py

---
+++ operacao_aritmeticas.py
@@ -0,0 +1,17 @@
+
+soma = 3 + 4 # Realiza a soma entre os 2 valores
+subtracao = 5 - 3 # Realiza a subtração entre os 2 valores
+multiplicacao = 5 * 3 # Realiza a multiplicação entre os 2 valores
+divisao_real = 5 / 2 # Realiza a divisão real entre os 2 valores
+divisao_inteira = 8 // 2 # Realiza a divisão inteira entre os 2 valores
+resto_da_divisao = 5 % 2 # Realiza o resto da divisão entre os 2 valores
+potencia = 5 ** 2 # Realiza a exponenciação
+
+
+print(soma)
+print(subtracao)
+print(multiplicacao)
+print(divisao_real)
+print(divisao_inteira)
+print(resto_da_divisao)
+print(potencia)
